d236dd275060ce3b407172acb0b22ef40d1a98d1
Dagaet <alojera15@gmail.com>
2023-11-12 20:37:58 +0000

    fix(Farmer): fixed error that make the thread infinite

--- src/net/salesianos/threads/Farmer.java
+++ src/net/salesianos/threads/Farmer.java
@@ -13,16 +13,18 @@ public class Farmer extends Thread {
     
     private Integer[] timers = {1,2,3,4,5,6,7,8,9,10,9,6,3,5,7};
 
-    int valueNumber = (int) Math.floor(Math.random() * vegetables.length);
-
+    
     @Override
     public void run() {
-        System.out.println("The vegetable " + vegetables[valueNumber] + " is growing. It will take " + timers[valueNumber] + " seconds.");
-        try {
-            sleep(timers[valueNumber] * 1000);
-            restaurant.getVegetables();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        while (restaurant.getVegetables() < 5) {
+            int valueNumber = (int) Math.floor(Math.random() * vegetables.length);
+            System.out.println("The vegetable " + vegetables[valueNumber] + " is growing. It will take " + timers[valueNumber] + " seconds.");
+            try {
+                sleep(timers[valueNumber] * 1000);
+                restaurant.receiveVegetables();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
